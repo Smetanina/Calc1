b722a35de973e43c970ea7de6a892944a50123f6
smetanina <smetanina@elewise.com>
2017-08-09 09:36:12 +0400

    Для сохраности

---
+++ EveryDay.Calc/Calculation/Models/DivOperation.cs
@@ -0,0 +1,20 @@
+﻿using EveryDay.Calc.Calculation.Interfaces;
+using System.Linq;
+
+namespace EveryDay.Calc.Calculation.Models
+{
+    public class DivOperation : IOperation
+    {
+        public double[] Input { get; set; }
+
+        public string Name
+        {
+            get { return "Div"; }
+        }
+
+        public double? GetResult()
+        {
+            return Input[0] / Input [1];
+        }
+    }
+}
---
+++ EveryDay.Calc/Calculation/Models/SqrtOperation.cs
@@ -0,0 +1,21 @@
+﻿using EveryDay.Calc.Calculation.Interfaces;
+using System;
+using System.Linq;
+
+namespace EveryDay.Calc.Calculation.Models
+{
+    public class SqrtOperation : IOperation
+    {
+        public double[] Input { get; set; }
+
+        public string Name
+        {
+            get { return "Sqrt"; }
+        }
+
+        public double? GetResult()
+        {
+            return Math.Sqrt(Input[0]);
+        }
+    }
+}
